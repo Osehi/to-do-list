fa22efd0bc8bac47840cfb64a4a4a8a2bbd5ade8
Osehi <osehiaseehilen@gmail.com>
2019-10-01 21:06:53 +0100

    database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
